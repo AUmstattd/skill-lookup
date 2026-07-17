074d2c8b2b026c3048955a083ddf0ee70fe6b4f5
Adam Umstattd <AUmstattd@gmail.com>
2026-07-17 16:32:06 -0400

    Add LLM API Call

---
+++ llm.py
@@ -0,0 +1,33 @@
+import requests
+import os
+
+api_key = os.environ.get("ANTHROPIC_API_KEY")
+
+
+def ask_llm(prompt):
+    if api_key is None:
+        print("No API Key Found")
+        return None
+    url = "https://api.anthropic.com/v1/messages"
+    headers = {
+        "content-type": "application/json",
+        "anthropic-version": "2023-06-01",
+        "x-api-key": api_key,
+    }
+    body = {
+        "model": "claude-haiku-4-5-20251001",
+        "max_tokens": 100,
+        "messages": [
+            {"role": "user", "content": prompt}
+        ],
+    }
+    response = requests.post(url, headers=headers, json=body)
+    if response.status_code == 200:
+        data = response.json()
+        return data['content'][0]['text']
+    else:
+        print(response.status_code, response.text)
+        return None
+
+answer = ask_llm("test")
+print(answer)
