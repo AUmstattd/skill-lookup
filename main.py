f53cb51315858f4cb047e724165867b1650400e4
Adam Umstattd <AUmstattd@gmail.com>
2026-07-18 14:52:17 -0400

    Update main with dunder

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import fetch_user
 import llm
-import sys
 
-username = input("Which Github User? ")
-print(f"fetching: {username}...")
-user = fetch_user.fetch_user(username)
-if user is None:
-    print("Could not find the username")
-    sys.exit()
-explain = llm.ask_llm(f"Explain this user in two, plain english sentences. Focus on their activity. Don't mention missing profile fields: {user}")
-if explain is None:
-    print("Could not get a summary")
-    sys.exit()
-print(explain)
+def main():
+    username = input("Which Github User? ")
+    print(f"fetching: {username}...")
+    user = fetch_user.fetch_user(username)
+    if user is None:
+        print("Could not find the username")
+        return
+    explain = llm.ask_llm(f"Explain this user in two, plain english sentences. Focus on their activity. Don't mention missing profile fields: {user}")
+    if explain is None:
+        print("Could not get a summary")
+        return
+    print(explain)
+
+if __name__ == "__main__":
+    main()
